2aebc5db591319984a799e69f0be1cbd8daa7e46
Martin Rode <martin.rode@programmfabrik.de>
2021-08-11 15:02:17 +0200

    Changed args for Snippet function, mark line with ">"

--- snippet.go
+++ snippet.go
@@ -4,20 +4,36 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"strconv"
 )
 
-// Snippet returns lines from-to, prefixed with the line number
-func Snippet(bts []byte, from, to int) (snipped []byte) {
+// Snippet returns lines from lineNo-plusMinus to lineNo+plusMinus, prefixed with the line number
+func Snippet(bts []byte, lineNo, plusMinus int) (snipped []byte) {
 	snipped = []byte{}
 	scanner := bufio.NewScanner(bytes.NewReader(bts))
 	line := 0
+	from := lineNo - plusMinus
+	to := lineNo + plusMinus
+
+	fromLen := len(strconv.Itoa(from))
+	toLen := len(strconv.Itoa(to))
+
+	pad := fromLen
+	if toLen > fromLen {
+		pad = toLen
+	}
+
 	for scanner.Scan() {
 		line++
 		if line > to {
 			break
 		}
 		if line >= from && line <= to {
-			snipped = append(snipped, []byte(fmt.Sprintf("%5d: %s\n", line, scanner.Text()))...)
+			gt := " "
+			if line == lineNo {
+				gt = "➜"
+			}
+			snipped = append(snipped, []byte(fmt.Sprintf("%s%"+strconv.Itoa(pad+1)+"d: %s\n", gt, line, scanner.Text()))...)
 		}
 	}
 	return snipped
