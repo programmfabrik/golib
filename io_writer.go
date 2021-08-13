c0d2c75a60d130f6e7562415a60aaedf69a66438
Martin Rode <martin.rode@programmfabrik.de>
2021-08-13 15:40:20 +0200

    IOWriter: Added wrapper for stdlib log to a io.Writer

--- io_writer.go
+++ io_writer.go
@@ -1,7 +1,19 @@
 package lib
 
+import (
+	"io"
+	"log"
+)
+
 type IOWriter func([]byte) (int, error)
 
 func (iow IOWriter) Write(bts []byte) (n int, err error) {
 	return iow(bts)
+}
+
+func IOWriterFromLogger(l *log.Logger) io.Writer {
+	return IOWriter(func(bts []byte) (int, error) {
+		l.Print(string(bts))
+		return len(bts), nil
+	})
 }
