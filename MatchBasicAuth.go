f0760432602be69e560338ea45e4d19172357add
Martin Rode <martin.rode@programmfabrik.de>
2021-08-16 14:50:06 +0200

    MatchBasicAuth: match map[string]string against provided http basic auth

---
+++ MatchBasicAuth.go
@@ -0,0 +1,41 @@
+package lib
+
+import (
+	"encoding/base64"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// MatchBasicAuth checks if a request contains credentials matched in basicAuth map.
+// "auth" is the http request header Authorization
+// It returns true if matched, false otherwise. If basicAuth is empty, this check returns true.
+func MatchBasicAuth(auth string, basicAuth map[string]string) bool {
+
+	s := strings.SplitN(auth, " ", 2)
+	if len(s) != 2 {
+		return false
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return false
+	}
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return false
+	}
+
+	for user, pass := range basicAuth {
+		err := bcrypt.CompareHashAndPassword([]byte(pass), []byte(pair[1]))
+		// if err != nil {
+		// 	println(err.Error())
+		// }
+		if pair[0] == user && err == nil {
+			return true
+		}
+	}
+
+	return false
+}
