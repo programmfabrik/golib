83357ad1b358b191ee740bad09d1e8316fba67a3
Martin Rode <martin.rode@programmfabrik.de>
2021-08-20 12:26:01 +0200

    New UniqueStrings

---
+++ UniqueStrings.go
@@ -0,0 +1,13 @@
+package lib
+
+// UniqueStrings returns all unique strings
+func UniqueStrings(in []string) (out []string) {
+	sMap := map[string]bool{}
+	for _, s := range in {
+		if !sMap[s] {
+			out = append(out, s)
+		}
+		sMap[s] = true
+	}
+	return out
+}
