9d0df4dbeba5270edcc272370df34af2cd6508e1
Martin Rode <martin.rode@programmfabrik.de>
2022-04-11 14:28:49 +0200

    Recover: Do not catch panic within panic callback
    This avoids hidden panics which are better thrown, so the caller sees there is a problem.

--- recover.go
+++ recover.go
@@ -4,7 +4,6 @@ import (
 	"runtime/debug"
 
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 )
 
 // Recover can be used in defer to execute "f" on recover
@@ -18,13 +17,7 @@ func Recover(f func(err error)) {
 	debug.PrintStack()
 	if f != nil {
 		func() {
-			defer func() {
-				r2 := recover()
-				if r2 != nil {
-					logrus.Errorf("Panic inside recover: %v", r2)
-					logrus.Debugf(string(debug.Stack()))
-				}
-			}()
+			// if f throws a panic we will not catch it here so the caller knows
 			err2, ok := r.(error)
 			if ok {
 				f(err2)
