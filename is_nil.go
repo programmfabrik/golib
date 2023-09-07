00c042335e1e711543f0bcae38a2e37ef424d3fc
Martin Rode <martin.rode@programmfabrik.de>
2023-09-07 09:19:38 +0200

    New func is_nil which works on "t any"

---
+++ is_nil.go
@@ -0,0 +1,17 @@
+package golib
+
+import "reflect"
+
+func IsNil(i any) bool {
+	if i == nil {
+		return true
+	}
+
+	v := reflect.ValueOf(i)
+	switch v.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.UnsafePointer, reflect.Interface, reflect.Slice:
+		return v.IsNil()
+	}
+
+	return false
+}
