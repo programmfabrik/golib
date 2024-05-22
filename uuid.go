70be4a7c2b41c83dfb4c94187216b27a076e5e9b
Martin Rode <martin.rode@programmfabrik.de>
2024-05-22 12:54:09 +0800

    Added "IsUUID"

--- uuid.go
+++ uuid.go
@@ -4,10 +4,23 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+// NewUUID returns a randomly generated UUID
 func NewUUID() string {
 	uuid, err := uuid.NewV4()
 	if err != nil {
 		panic(err)
 	}
 	return uuid.String()
+}
+
+// IsUUID returns true if id is the same format as NewUUID provides
+func IsUUID(id string) bool {
+	uid, err := uuid.FromString(id)
+	if err != nil {
+		return false
+	}
+	if uid.Version() != uuid.V4 {
+		return false
+	}
+	return true
 }
