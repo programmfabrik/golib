b3fb107e50696bfb5077e421c11c68ada70badd7
Martin Rode <martin.rode@programmfabrik.de>
2025-01-15 16:00:17 +0100

    Route: fixed escaped path

--- http_route.go
+++ http_route.go
@@ -11,22 +11,26 @@ import (
 // escaped form of the path, so a path like /files/he%2Fnk/more will keep the
 // %2F.
 func Subroute(req *http.Request) string {
+	// Pln("route: %s", Route(req))
+	// Pln("rout2: %s", url.PathEscape(Route(req)))
+	// Pln("esc  : %s", req.URL.EscapedPath())
 	return req.URL.EscapedPath()[len(Route(req)):]
 }
 
-// Route returns the route from a Gorilla Mux
+// Route returns the route from a Gorilla Mux. It uses the escaped
+// form of the path.
 func Route(req *http.Request) string {
 	route := mux.CurrentRoute(req)
 	if route == nil {
 		return req.URL.Path
 	}
 	routeRegex, err := route.GetPathRegexp()
-
 	if err != nil {
 		panic(err)
 	}
 	reg := regexp.MustCompile(routeRegex)
-	match := reg.FindString(req.URL.Path)
-	// logrus.Debugf("Route: %s %v", routeRegex, match)
-	return req.URL.Path[:len(match)]
+	escPath := req.URL.EscapedPath()
+	match := reg.FindString(escPath)
+	// Pln("Route: %s\nMatch: %v\ncut: %q", routeRegex, match, escPath[:len(match)])
+	return escPath[:len(match)]
 }
